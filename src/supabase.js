90e3f8f3404495f13bef54a41e2912916fd4f24c
Erick Njiru <blankops117@gmail.com>
2025-12-06 12:37:05 +0300

    Clean static HTML version - remove all React dependencies

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-  "https://xcszytfkhmvdbroidenx.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhjc3p5dGZraG12ZGJyb2lkZW54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwMDA0MDUsImV4cCI6MjA4MDU3NjQwNX0.BONal6h5liK2H9_GzlQvOIY_KHgg3HwpY5MqUnS6XVQ"
-);
